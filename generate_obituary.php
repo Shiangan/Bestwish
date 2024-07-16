3a687a36dcd3f9093443aed4b6d1ab1a76fc11f6
Shiangan <avarubby85110@gmail.com>
2024-07-16 13:42:14 +0800

    Update generate_obituary.php

--- generate_obituary.php
+++ generate_obituary.php
@@ -1,6 +1,9 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
+    $deceasedName = $_POST['deceasedName'];
+    $birthDate = $_POST['birthDate'];
+    $deathDate = $_POST['deathDate'];
+    $funeralLocation = $_POST['funeralLocation'];
     $relationship = $_POST['relationship'];
     $email = $_POST['email'];
     
@@ -8,7 +11,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // 例如，檢查並清理輸入的資料
     
     // 生成訃聞內容
-    $obituary = "親愛的 $relationship $name 您好，感謝您的填寫。\n\n這是一段訃聞文字內容，用來描述逝者的生平和相關資訊。\n\n如有需要，請聯繫我們。";
+    $obituary = "親愛的 $relationship 您好，感謝您的填寫。\n\n在 $deathDate ，我們緬懷 $deceasedName ，我們將永遠怀念那个纯洁的心灵。\n\n奠禮地點：$funeralLocation。\n\n如有需要，請聯繫我們。";
 
     // 將訃聞內容顯示或儲存到資料庫，或返回給前端顯示
     echo $obituary;
